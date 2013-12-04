2d9ed94c0778bdffda5fa24fb8dab4412e8fad6d
Sieurin Aurelien <asieurin@margaux.educ.insa>
2013-12-04 14:36:44 +0100

    Mon premier commit

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,17 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	/**
+	 * Ceci est mon main très important donc je mets un commentaire
+	 * @param args
+	 */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
